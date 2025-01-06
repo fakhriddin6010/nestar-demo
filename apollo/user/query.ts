050c5a7fad5f7b8de48b5eef4a8d5cb6ffb1ce4f
fakhriddin <faxa99deni@gmail.com>
2025-01-06 23:55:12 +0900

    feat: develop apollo client connection

---
+++ apollo/user/query.ts
@@ -0,0 +1,83 @@
+import { gql } from "@apollo/client";
+
+/*************************
+ *   PROPERTY    *
+ ************************/
+
+export const GET_PROPERTIES = gql`
+  query GetProperties($input: PropertiesInquiry!) {
+    getProperties(input: $input) {
+      list {
+        _id
+        propertyType
+        propertyLocation
+        propertyStatus
+        propertyAddress
+        propertyTitle
+        propertyPrice
+        propertySquare
+        propertyBeds
+        propertyRooms
+        propertyViews
+        propertyLikes
+        propertyComments
+        propertyRank
+        propertyImages
+        propertyDesc
+        propertyBarter
+        propertyRent
+        memberId
+        soldAt
+        deletedAt
+        constructedAt
+        createdAt
+        updatedAt
+        memberData {
+          _id
+          memberType
+          memberStatus
+          memberAuthType
+          memberPhone
+          memberNick
+          memberFulName
+          memberImage
+          memberAddress
+          memberDesc
+          memberProperties
+          memberArticles
+          memberFollowers
+          memberFollowings
+          memberPoints
+          memberLikes
+          memberViews
+          memberComments
+          memberRank
+          memberWarnings
+          memberBlocks
+          deletedAt
+          createdAt
+          updatedAt
+          accessToken
+          meLiked {
+            memberId
+            likeRefId
+            myFavorite
+          }
+          meFollowed {
+            followingId
+            followerId
+            myFollowing
+          }
+        }
+        meLiked {
+          memberId
+          likeRefId
+          myFavorite
+        }
+      }
+      metaCounter {
+        total
+      }
+    }
+  }
+`;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
+import client from "@/apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import type { AppProps } from "next/app";
 import { useState } from "react";
 import { light } from "../scss/MaterialTheme";
 import "../scss/app.scss";
-import "../scss/pc/main.scss"
-import "../scss/mobile/main.scss"
+import "../scss/mobile/main.scss";
+import "../scss/pc/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
@@ -13,9 +15,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   // Socket.io, Redux, Mui ...
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
